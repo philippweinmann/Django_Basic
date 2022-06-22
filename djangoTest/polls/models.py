14b8095f18475e6e03a70299088364435c1e4f52
Philipp Weinmann <philipp.weinmann71@gmail.com>
2022-06-22 12:49:24 +0200

    deleting model choices because fuck the official documentation

--- djangoTest/polls/models.py
+++ djangoTest/polls/models.py
@@ -9,9 +9,3 @@ class Question(models.Model):
 class Statement(models.Model):
     statement_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
