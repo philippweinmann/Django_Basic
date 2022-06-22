6fa7f6cfd3f24e67d214e53079884b690292ea0d
Philipp Weinmann <philipp.weinmann71@gmail.com>
2022-06-22 12:48:40 +0200

    added statements model

--- djangoTest/polls/models.py
+++ djangoTest/polls/models.py
@@ -6,6 +6,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 
+class Statement(models.Model):
+    statement_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
--- djangoTest/polls/urls.py
+++ djangoTest/polls/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 from .question_api import QuestionViewSet
+from .statement_api import StatementViewSet
 
 router = DefaultRouter()
 router.register(r'questions', QuestionViewSet, basename='questions')
+router.register(r'statements', StatementViewSet, basename='statements')
 
 urlpatterns = router.urls
