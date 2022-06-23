04cbebbf9ae66953cc6d6dafc2624eb5ddd1de76
Philipp Weinmann <philipp.weinmann71@gmail.com>
2022-06-23 11:44:02 +0200

    separated serializer into read and write

--- djangoTest/polls/sentence_api.py
+++ djangoTest/polls/sentence_api.py
@@ -3,15 +3,25 @@ from rest_framework import serializers, viewsets
 from .models import Sentence
 
 
-class SentenceSerializer(serializers.ModelSerializer):
+class SentenceSerializerRead(serializers.ModelSerializer):
+    class Meta:
+        model = Sentence
+        fields = "__all__"
 
+
+class SentenceSerializerWrite(serializers.ModelSerializer):
     class Meta:
         model = Sentence
         fields = "__all__"
 
 
 class SentenceViewSet(viewsets.ModelViewSet):
-    serializer_class = SentenceSerializer
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'list']:
+            return SentenceSerializerRead
+        else:
+            return SentenceSerializerWrite
+
     queryset = Sentence.objects.all()
 
     def list(self, request, *args, **kwargs):
